738aa07e59322ab76ff0d89484f992e8492a3607
root <root@tararoys.(none)>
2013-03-10 01:12:40 +0000

    Display of gameboard extracted to method
    note: As a teacher sometiems it's best to let your students
    make the mistakes.  That way they learn from it and we
    all have a good laugh.
    
    note: and sometimes the teacher likes to giggle as a student
    does not spot the obvious bug for ten minutes because she
    'knew' where to look for it.

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,17 @@
 
+def show_gameboard
+  gameboard = [8, 1, 6, 3, 5, 7, 4, 9, 2]
+  puts gameboard[0].to_s+" "+gameboard[1].to_s+" "+gameboard[2].to_s
+  puts gameboard[3].to_s+" "+gameboard[4].to_s+" "+gameboard[5].to_s
+  puts gameboard[6].to_s+" "+gameboard[7].to_s+" "+gameboard[8].to_s
+end 
+
 9.times do |num| 
   if num.odd?
     puts "Player X Turn"
   else 
     puts "Player O turn" 
   end
-  puts "8 1 6"
-  puts "3 5 7"
-  puts "4 9 2"
+  show_gameboard
   puts "Choose a number"
-  number = gets 
 end
