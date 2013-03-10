c7d3f2d067a174bb2b4412a320936a2ead1c68d0
root <root@tararoys.(none)>
2013-03-10 00:45:37 +0000

    Game board displays for each player turn

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,13 @@
 
-7.times do |num| 
+9.times do |num| 
   if num.odd?
-    puts "Player X Turn" 
+    puts "Player X Turn"
   else 
     puts "Player O turn" 
   end
+  puts "8 1 6"
+  puts "3 5 7"
+  puts "4 9 2"
+  puts "Choose a number"
+  number = gets 
 end
