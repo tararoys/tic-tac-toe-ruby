6cf396095af7bb1c29dd322480266935ea761cd8
root <root@tararoys.(none)>
2013-03-10 03:08:19 +0000

    Command line versus mode tic tac toe
    Features added:  ability to choose space on tic-tac-toe board,
    x and o marks show up on board.  It is now possible to play
    versus tic tac toe.

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,24 @@
 
-def show_gameboard
-  gameboard = [8, 1, 6, 3, 5, 7, 4, 9, 2]
+
+def show_gameboard(gameboard)
   "#{gameboard[0]} #{gameboard[1]} #{gameboard[2]}\n" + 
   "#{gameboard[3]} #{gameboard[4]} #{gameboard[5]}\n" +
   "#{gameboard[6]} #{gameboard[7]} #{gameboard[8]}"
 end 
 
-9.times do |num| 
+
+gameboard = [8, 1, 6, 3, 5, 7, 4, 9, 2]
+9.times do |num|   
   if num.odd?
     puts "Player X Turn"
+    marker = "x"
   else 
     puts "Player O turn" 
+    marker = "O"
   end
-  show_gameboard
+  display = show_gameboard(gameboard)
+  puts display
   puts "Choose a number"
+  board_number = gets.to_i
+  gameboard[gameboard.index(board_number)] = marker
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,6 +3,6 @@ require "game"
 describe "gameboard output" do 
 
   it "looks like this wheeee!"  do    
-    expect(show_gameboard).to eq("8 1 6\n3 5 7\n4 9 2")
+    expect(show_gameboard([8,1,6,3,5,7,4,9,2])).to eq("8 1 6\n3 5 7\n4 9 2")
   end
 end
