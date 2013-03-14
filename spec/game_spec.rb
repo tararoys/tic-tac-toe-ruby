11c47e10b7dc33ae3a3103542bc2f06477623aca
root <root@tararoys.(none)>
2013-03-14 05:35:54 +0000

    extra test for winning move method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,15 +1,22 @@
 require "game"
 
 describe "Winning Move" do
-  before do 
-    @player = ComputerPlayer.new("x")
-    @player.moves=[8, 1]
-    @game = Game.new
-    @game.stub(:gameboard).and_return(['x','x',6,'O','O',7,4,9,2])
-  end
   it "chooses the winning move" do
-    win = @player.winning_move(@game)
+    player = ComputerPlayer.new("x")
+    player.moves=[8, 1]
+    game = Game.new
+    game.stub(:gameboard).and_return(['x','x',6,'O','O',7,4,9,2])
+    win = player.winning_move(game)
     expect(win).to eq(6) 
+  end
+
+  it "chooses the winning move" do
+    player = ComputerPlayer.new("O")
+    player.moves=[5, 3]
+    game = Game.new
+    game.stub(:gameboard).and_return(['x','x',6,'O','O',7,4,9,2])
+    win = player.winning_move(game)
+    expect(win).to eq(7) 
   end
 end
 
